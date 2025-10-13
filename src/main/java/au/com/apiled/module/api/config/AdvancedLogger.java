92889c3d172baf95e33d8fec8ce9de5060b15b93
Adilson Dias <140462990+adilsondias-engineer@users.noreply.github.com>
2025-10-13 19:10:10 +1100

    updated logo

--- src/main/java/au/com/apiled/module/api/config/AdvancedLogger.java
+++ src/main/java/au/com/apiled/module/api/config/AdvancedLogger.java
@@ -6,7 +6,7 @@ public class AdvancedLogger {
 	 public static final String  PREFIX = "advanced-logger";
 	 public static final String  MODULE_NAME= PREFIX;
 	 public static final String  MODULE_DESCRIPTION ="Advanced Logger";
-	 public static final String  MODULE_VERSION ="1.0.0-SNAPSHOT";
+	 public static final String  MODULE_VERSION ="1.0.2-SNAPSHOT";
 	 public static final String  MODULE_VENDOR = "API-Led Pty Ltd";
 	 public static final String  MODULE_XSD_FILE_NAME= "advanced-logger-modules.xsd";
 	 public static final String  MODULE_SCHEMA_LOCATION="http://www.api-led.com.au/modules/advanced-logger-modules.xsd";
